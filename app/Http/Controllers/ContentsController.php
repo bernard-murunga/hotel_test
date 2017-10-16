345d542603f02c13ff06eaa62bd5ae030d2f6aa5
bwmurunga <bwmurunga@gmail.com>
2017-10-16 22:40:04 +0300

    image upload

--- app/Http/Controllers/ContentsController.php
+++ app/Http/Controllers/ContentsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 
 class ContentsController extends Controller
 {
@@ -14,5 +15,22 @@ class ContentsController extends Controller
         $last_updated = $request->session()->has('last_updated')? $request->session()->pull('last_updated') : 'none';
         $data['last_updated'] = $last_updated;
         return view('contents/home', $data);
+    }
+
+    public function upload(Request $request)
+    {
+        $data = [];
+        if($request->isMethod('post'))
+        {
+          $this->validate(
+            $request,
+            [
+              'image_upload' => 'mimes:jpeg,bmp,png'
+            ]
+          );
+          Input::file('image_upload')->move('images'.'attractions.jpg');
+          return redirect('/');
+        }
+        return view('contents/upload', $data);
     }
 }
